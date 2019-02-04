d0adc1f32a8f0b4e5c8c0256736c33ab985fa67d
natinusala <natinusala@gmail.com>
2019-02-04 18:18:51 +0100

    remove root safety check (oh no)

--- src/bbfs.c
+++ src/bbfs.c
@@ -888,10 +888,12 @@ int main(int argc, char *argv[])
     // and refuse if it is.  The somewhat smaller hole of an ordinary
     // user doing it with the allow_other flag is still there because
     // I don't want to parse the options string.
-    if ((getuid() == 0) || (geteuid() == 0)) {
+
+    //Except that Lakka only has root so let me do what I want ffs
+    /*if ((getuid() == 0) || (geteuid() == 0)) {
     	fprintf(stderr, "Running BBFS as root opens unnacceptable security holes\n");
     	return 1;
-    }
+    }*/
 
     // See which version of fuse we're running
     fprintf(stderr, "Fuse library version %d.%d\n", FUSE_MAJOR_VERSION, FUSE_MINOR_VERSION);
